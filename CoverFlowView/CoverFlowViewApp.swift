72471c42952480207911f6bbdab60add816a84d6
sauna1137 <soneko2102@gmail.com>
2024-12-09 11:09:50 +0900

    Initial Commit

---
+++ CoverFlowView/CoverFlowViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverFlowViewApp.swift
+//  CoverFlowView
+//
+//  Created by KS on 2024/12/09.
+//
+
+import SwiftUI
+
+@main
+struct CoverFlowViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoverFlowViewTests/CoverFlowViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoverFlowViewTests.swift
+//  CoverFlowViewTests
+//
+//  Created by KS on 2024/12/09.
+//
+
+import Testing
+@testable import CoverFlowView
+
+struct CoverFlowViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
